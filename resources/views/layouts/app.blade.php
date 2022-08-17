2f4dee664fad82fed436b10f46dc47c1ba4ba201
Stijn Bouckaert <stijnbouckaert@Stijns-MacBook-Pro.local>
2022-08-17 23:21:45 +0200

    setup[page]: header+footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,37 @@
 
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
 </head>
+<header>
+    <div class="header header--primary">
+        <div class="header__component">
+            <p>Sellery</p>
+        </div>
+        <div class="header__component header__component--last">
+            @if(Auth::id())
+                <a href="/logout" class="btn btn--danger">logout</a>
+            @else
+                <a href="/login" class="btn btn--success">login</a>
+            @endif
+        </div>
+    </div>
+    @if(Auth::id())
+    <div class="header header--secondary">
+        <div class="header__component">
+            <p>{{Auth::user()->name}}</p>
+        </div>
+        <div class="header__component header__component--last">
+            <a href="/products/create" class="btn btn--success">Zoekertje plaatsen</a>
+        </div>
+    </div>
+    @endif
+</header>
 <body>
 
     @yield('content', 'Nothing to see here')
+    <footer>
+        <p>Sellery is niet aansprakelijk voor verkeerd gebruik van zijn services.</p>
+        <p>Copyright © 2022 Sellery. Alle rechten voorbehouden.</p>
+    </footer>
     <livewire:scripts />
 </body>
 </html>
